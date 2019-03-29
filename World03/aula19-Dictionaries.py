3d23aca4c848dc1292fca80e37c427b455835ac4
Ricardo Benhossi <ricardo.benhossi@gmail.com>
2019-03-29 13:05:34 -0300

    Class 19 Dictionary

---
+++ World03/aula19-Dictionaries.py
@@ -0,0 +1,13 @@
+# To start a dictionay just name = {} or name = dict()
+pessoas = {'nome': 'Gustavo', 'sexo': 'M', 'idade': 22}
+print(pessoas)
+print(pessoas.items())
+print(pessoas.keys())
+print(pessoas.values())
+# print(pessoas[0]) # ERROR no index 0
+print(pessoas['nome'])
+print(f'O {pessoas["nome"]} tem {pessoas["idade"]}.')
+
+# Add new items: just add as a regurlar variable.
+pessoas['altura']
+prin
