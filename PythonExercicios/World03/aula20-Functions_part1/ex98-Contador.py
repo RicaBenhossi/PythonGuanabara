1e271e4b5dc7ce568847971328b60c866998b215
ricardo <ricardo@logusretail.com.br>
2019-04-23 19:37:38 -0300

    ex098 enhaced

--- PythonExercicios/World03/aula20-Functions_part1/ex98-Contador.py
+++ PythonExercicios/World03/aula20-Functions_part1/ex98-Contador.py
@@ -2,16 +2,24 @@ from time import sleep
 
 
 def contador(inicio, fim, passo):
-    print('-=' * 50)
-    print(f'Contagem de {inicio} até {fim} de {passo} em {passo}')
-
     if passo == 0:
         passo += 1
 
+    print('-=' * 50)
+    print(f'Contagem de {inicio} até {fim} de {abs(passo)} em {abs(passo)}')
     if inicio > fim:
-        passo *= -1
+        fim -= 1
+        if passo > 0:
+            passo *= -1
+    elif inicio < fim:
+        fim += 1
+        if passo < 0:
+            passo *= -1
+    else:
+        print('Impossível contar. Mesmo início e fim.')
 
-    for x in range(inicio, fim + 1, passo):
+
+    for x in range(inicio, fim, passo):
         print(x, end=' ', flush=True)
         # inicio += passo
         sleep(0.5)
@@ -19,7 +27,7 @@ def contador(inicio, fim, passo):
     print('FIM!')
 
 
-contador(1, 10, 1)
-contador(10, 1, 2)
+# contador(1, 10, 1)
+# contador(10, 1, 2)
 print('Sua vez de fazer uma contagem')
 contador((int(input('Inicio: '))), (int(input('Fim: '))), (int(input('Passo: '))))
