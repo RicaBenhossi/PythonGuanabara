dd0064eb49039d0e4cf3139e2413feadad6bd816
ricardo <ricardo@logusretail.com.br>
2019-04-25 13:24:20 -0300

    exercise 100 completed

--- PythonExercicios/World03/aula20-Functions_part1/ex100-Sortea_Soma_Par.py
+++ PythonExercicios/World03/aula20-Functions_part1/ex100-Sortea_Soma_Par.py
@@ -1,22 +1,27 @@
 from random import randint
+from time import sleep
 
 
 def Sorteia(lista):
+    print('*' * 40)
+    print('Sorteando 5 números aleatório entre 1 e 10')
     for x in range(0, 5):
-        lista.append(randint(0, 5))
-    print(lista)
+        lista.append(randint(1, 10))
+        print(lista[x], end=' ', flush=True)
+        sleep(0.5)
+    print('-' * 40)
+    print(f'A Lista completa é {lista}')
 
 
-def SomaPar(numeros):
+def SomaPar(lista):
     soma_par = 0
-    Sorteia(numeros)
-    for num in numeros:
+    print('*' * 40)
+    for num in lista:
         if num % 2 == 0:
             soma_par += num
-    print(soma_par)
+    print(f'A Soma dos elementos pares da lista {lista} é {soma_par}.')
 
 
-print('*' * 40)
 lista_numeros = list()
-# Sorteia(lista_numeros)
+Sorteia(lista_numeros)
 SomaPar(lista_numeros)
