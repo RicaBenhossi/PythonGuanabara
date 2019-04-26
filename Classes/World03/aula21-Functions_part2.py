87d3f85c802f97e2f3ee8042d0284e21d5231349
Ricardo Benhossi <ricardo.benhossi@gmail.com>
2019-04-26 14:21:22 -0300

    test

--- Classes/World03/aula21-Functions_part2.py
+++ Classes/World03/aula21-Functions_part2.py
@@ -68,4 +68,6 @@ print(f'X after method = {x} -> X has not been changed because X inside variable
 print(f'Y after method = {y} -> Y has not been changed because Y was not used inside variable_scope (neither GLOBAL nor LOCAL).')
 
 print('=' * 40)
-teste31
+
+
+teste A1.B0
