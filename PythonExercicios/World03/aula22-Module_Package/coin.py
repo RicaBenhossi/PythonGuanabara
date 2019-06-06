d558b68dec1a1c88ff1d92ceb56da563733bc0e5
Ricardo Benhossi <ricardo.benhossi@gmail.com>
2019-06-06 06:58:27 -0300

    Exercise 110 complete

--- PythonExercicios/World03/aula22-Module_Package/coin.py
+++ PythonExercicios/World03/aula22-Module_Package/coin.py
@@ -33,3 +33,16 @@ def metade(num, formatado=False):
 # NOTE Exercise 108
 def format_coin(num):
     return f'R${num:.2f}'.replace('.', ',')
+
+
+# NOTE Exercise 110
+def resumo(num, aumento, desconto):
+    print('-' * 40)
+    print(f'{"RESUMO DO VALOR":^40}')
+    print('-' * 40)
+    print(f'Preço Analisado:{format_coin(num):.>24}')
+    print(f'Dobro do Preço:{dobro(num, True):.>25}')
+    print(f'Metade do Preço:{metade(num, True):.>24}')
+    print(f'{aumento}% de aumento:{aumentar(num, aumento, True):.>25}')
+    print(f'{desconto}% de desconto:{diminuir(num, desconto, True):.>24}')
+    print('-' * 40)
--- PythonExercicios/World03/aula22-Module_Package/results.py
+++ PythonExercicios/World03/aula22-Module_Package/results.py
@@ -1,8 +1,11 @@
 import coin
 
 
-price = float(input('Digite um preço: '))
-print(f'O dobro de {coin.format_coin(price)} é {coin.dobro(price)}')
-print(f'A metade de {coin.format_coin(price)} é {coin.metade(price)}')
-print(f'O preço acréscimo de 10% é {coin.aumentar(price, 10)}')
-print(f'O preço com desconto de 20% é {coin.diminuir(price, 20)}')
+# price = float(input('Digite um preço: '))
+price = 100
+print(f'O dobro de {coin.format_coin(price)} é {coin.dobro(price, True)}')
+print(f'A metade de {coin.format_coin(price)} é {coin.metade(price, True)}')
+print(f'O preço acréscimo de 10% é {coin.aumentar(price, 10, True)}')
+print(f'O preço com desconto de 20% é {coin.diminuir(price, 20, True)}')
+
+print(coin.resumo(price, 20, 10))
