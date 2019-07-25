d2456bf602d1391064ff96ddbe198dbf6a5fad84
Ricardo Benhossi <ricardo.benhossi@gmail.com>
2019-07-25 14:21:30 -0300

    ew class: Try Except

---
+++ Classes/World03/aula23-Error_Handling.py
@@ -0,0 +1,12 @@
+# NOTE Possible structure of try
+"""
+try:
+    # Validation you want to run
+except :
+    # Command to run if it doen't pass the validation
+else:
+    # If succeeded, execute this
+finally:
+    # This commands will be executed after Try finishes. Will be executed anyway.
+"""
+
