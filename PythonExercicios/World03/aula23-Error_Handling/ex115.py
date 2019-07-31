72e891b65db91654fb2eb2429b87e9041b1eaec8
RicaBenhossi <ricardo.benhossi@gmail.com>
2019-07-31 14:03:14 -0300

    Ex115: Scope of exercise.

---
+++ PythonExercicios/World03/aula23-Error_Handling/ex115.py
@@ -0,0 +1,8 @@
+"""
+# NOTE SCOPE
+Create a system that allows user to register name and age of a person and save in a text file.
+System should have a menu containing:
+   1 - Register a new user
+   2 - List all users registered
+   3 - Exit
+"""
