9270f1b04dd4ca0a174db30046de33430c06fc46
Ricardo Benhossi <ricardo.benhossi@gmail.com>
2019-08-01 22:30:53 -0300

    Organizing files of Exercise 115 into a folder.

---
+++ PythonExercicios/World03/aula23-Error_Handling/ex115-SystemNamesAges/ex115.py
@@ -0,0 +1,5 @@
+"""
+System is designed to save and list people names and age. Data are stored in a text file.
+"""
+
+
--- PythonExercicios/World03/aula23-Error_Handling/ex115.py
+++
@@ -1,4 +0,0 @@
-"""
-SCOPE
-Create a system that allows user to register name and age of a person and save in a text file.
-"""
