02eb1623ef37f4474fa43f570aa0bf77aef528a9
Ricardo Benhossi <ricardo.benhossi@gmail.com>
2019-09-12 18:45:08 -0300

    adjust to create a virtual environment.

--- Exercises/World03/aula23-Error_Handling/ex114-CheckWebPage.py
+++ Exercises/World03/aula23-Error_Handling/ex114-CheckWebPage.py
@@ -5,7 +5,6 @@ import urllib.request as url
 try:
     page = url.urlopen("http://ttt.com.br")
 except Exception as erro:
-    erro.
     print(f'Connection error! {erro.__class__}')
 else:
     browser.open_new_tab("http://pudim.com.br")
